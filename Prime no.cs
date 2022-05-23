f4261d752926dc384ddea46ea564541fb4edcd5c
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-23 15:25:48 +0530

    Add files via upload

---
+++ Prime no.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 7;
+            int count = 0;
+            for (int i = 1; i <=num; i++)
+            {
+                if (num% i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("prime no");
+            }
+            else
+            {
+                Console.WriteLine("not a prime no");
+            }       
+                    Console.ReadLine();
+                
+            
+        }
+    }
+}
+    
+
