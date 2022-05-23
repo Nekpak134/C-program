0a471ec78419a0fbb965dd0eeb868789f36e7f48
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-23 15:26:22 +0530

    Add files via upload

---
+++ string reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any string");
+            string s = Console.ReadLine();
+            Console.Write("reverse string =");
+            for(int i =(s.Length-1);i>=0; i--)
+            {
+                Console.Write(s[i]);
+            }
+             
+            Console.ReadLine();
+
+            
+        }
+    }
+}
