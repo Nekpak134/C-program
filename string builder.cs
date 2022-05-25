74882dd74dbbc75ffb837afc7e095ae6c8ab8583
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-26 03:12:39 +0530

    Add files via upload

---
+++ string builder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder obj = new StringBuilder("nekpal  ");
+            obj.AppendLine("kumar");
+            Console.WriteLine(obj);
+         
+            Console.ReadLine();
+        }
+    }
+}
