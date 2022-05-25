1091bf6bedb86c9f364f7bf1bfa418d2990a6e23
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-26 03:05:51 +0530

    Add files via upload

---
+++ break or continue .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          /*for(int i = 0; i <=10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 7)
+                    break;
+            }
+            Console.WriteLine("after the loop");
+            Console.ReadLine();*/
+
+
+            for(int i = 0; i <=10; i++)
+            {
+                
+                if (i == 8)
+                    continue;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("after the loop");
+            Console.ReadLine();
+            
+        }
+    }
+}
