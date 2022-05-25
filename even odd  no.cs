8afe1d5634ea335595228894f084892ac38462a5
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-26 03:06:18 +0530

    Add files via upload

---
+++ even odd  no.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <=100; i++)
+            {
+                if(i%2==0)
+               Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+           /* Console.WriteLine("enter the no to test");
+            int i=Convert.ToInt32(Console.ReadLine());
+            if(i%2==0)
+            {
+                Console.WriteLine("Even no");
+            }
+            else
+            {
+                Console.WriteLine("odd no");
+            }
+            Console.ReadLine();*/
+        }
+    }
+}
