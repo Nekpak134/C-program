9893e600290833308b0651929223ee45102b7267
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-26 03:10:56 +0530

    Add files via upload

---
+++ for loop  .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* for(int i = 0; i <=100; i++)
+             {
+                 Console.WriteLine(i);
+             }
+                Console.ReadLine();*/
+
+            Console.WriteLine("i j");
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    Console.WriteLine(i + " " + j);
+                }
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
