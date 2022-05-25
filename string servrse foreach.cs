0edce887918d33e7f53334707cfc9b8d6307d081
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-26 03:08:39 +0530

    Add files via upload

---
+++ string servrse foreach.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s1 = "welcom to chetu";
+            string s2 = "";
+            foreach(var  item in s1)
+            {
+                s2 = item + s2;
+
+            }
+            Console.WriteLine(s2);
+            Console.ReadLine();
+        }
+    }
+}
