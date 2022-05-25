e5e701036f506b1497642f4f9c2c037bb3e7d691
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-26 03:11:39 +0530

    Add files via upload

---
+++ if else grade  .cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("enter the no to test");
+            int i=Convert.ToInt32(Console.ReadLine());
+            if(i>=100)
+            {
+                Console.WriteLine("a greade");
+            }
+            else if (i>=80)
+            {
+                Console.WriteLine("b greade");
+            }
+            else if (i>=50)
+            {
+                Console.WriteLine("c greade ");
+            }
+            else
+            {
+                Console.WriteLine("fal");
+            }
+               Console.ReadLine();
+        }
+    }
+}
