36f72bceb775b1697e13b4362e3980419e14d7db
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-26 03:10:23 +0530

    Add files via upload

---
+++ fiboncei no.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+
+
+            /*int a = 1;
+            int b = 2;
+            int c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i <= 10; i++)
+            { 
+                c = a + b;
+            Console.WriteLine(c);
+            a = b;
+            b= c;
+            }
+            Console.ReadLine();*/
+        }
+    }
+}
