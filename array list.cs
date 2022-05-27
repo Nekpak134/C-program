4ee388b888933f348502658ace3d106cf2ea0adb
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-27 16:45:39 +0530

    Add files via upload

---
+++ array list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cars = new List<string>();
+            cars.Add("nekpal");
+            cars.Add("rihan");
+            cars.Add("sachin");
+            cars.Add("nadim");
+            foreach (string car in cars)
+            //for (int i = 1; i <= cars.Count; i++)
+            {
+                 Console.WriteLine(car);
+            }
+                Console.ReadLine();
+            
+
+        }
+    }
+}
