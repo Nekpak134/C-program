76a5489ef5a42dbfa62ddab33e08021c90aa85b1
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-27 17:03:38 +0530

    Add files via upload

---
+++ methad 3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+     class Program
+    {
+       /* public void mymethad(int i, int j)
+        {
+            int result = i + j;
+            Console.WriteLine(result);
+            
+        }
+        public static void Main(string[] args)
+        {
+            Program m = new Program();
+             m.mymethad(90, 90);
+           Console.ReadLine();*/
+        static void mymethad(string neme,int age)
+        {
+            Console.WriteLine(neme+"is "+age);
+        }
+        static void Main(string[]args)
+        {
+            mymethad("nekpal ", 20);
+            mymethad("rihan ", 22);
+            Console.ReadLine();
+
+        }
+    }
+}
