10b6c9c0ffddb6427f887c587bfd9fa0a78c52af
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-27 16:59:48 +0530

    Add files via upload

---
+++ array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] cars = new string[] { "nekpal", "rihan", "sachin" };
+            Console.WriteLine(cars[0]);
+          
+                Console.ReadLine();
+            
+        }
+    }
+}
