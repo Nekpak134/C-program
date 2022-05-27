ee5eb48dc13641dbe292469e035edce2c07a52d7
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-27 16:44:52 +0530

    Add files via upload

---
+++ array for loop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] cars = new string[] { "nekpal", "rihan", "sachin" };
+           // for (int i = 0; i <= 9; i++)
+                foreach(string car in cars)
+            {
+                Console.WriteLine(cars[1]);
+            }
+          
+                Console.ReadLine();
+            
+        }
+    }
+}
