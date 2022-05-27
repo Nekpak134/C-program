eb7ac5ed0280e8b1b7d13831cf0728ad74804aad
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-27 17:02:27 +0530

    Add files via upload

---
+++ methad 2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+     class Program
+    {
+        public void mymethad()
+        {
+            Console.WriteLine("i am good boy");
+        }
+        public static void Main(string[] args)
+        {
+            Program m = new Program();
+            m.mymethad();
+           Console.ReadLine();
+        }
+    }
+}
