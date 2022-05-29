98f971f16340f39aafcead9076f2a465212938ed
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-29 12:39:00 +0530

    Add files via upload

---
+++ overloding 2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Program
+    {
+        public void m1(int a)
+        {
+            Console.WriteLine(a+"  methad with integert");
+        }
+        public void m1(float f)
+        {
+            Console.WriteLine(f+"    methad with float");
+        }
+    }
+    class B
+    {
+        public static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.m1(4);
+            obj.m1(5.5f);
+            Console.ReadLine();
+
+        }
+    }
+}          
+
+
+     
