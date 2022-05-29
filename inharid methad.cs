312d96bf24530e3d53bfdedb636e4abc5e9b4412
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-05-29 12:35:12 +0530

    Add files via upload

---
+++ inharid methad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Program
+    {
+        static void m1(string child1, string chidl2, string chidl3)
+        {
+            Console.WriteLine("your name is :" + chidl2);
+        }
+        static void Main(string[] args)
+        {
+            m1( child1:"nekpal",chidl2:"rihan",chidl3: "sachin");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
