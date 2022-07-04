fd7b19c0eec0d3d6b2762d551d6620a59b2179a4
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-07-04 23:49:42 +0530

    Add files via upload

---
+++ Pattern4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                int x, y, z;
+                for (x = 1; x <= 8; x++)
+                {
+                    for (y = 1; y <= x; ++y)
+                        Console.Write(y);
+
+                    for (z = 8 - x; z >= 1; z--)
+                        Console.Write("*");
+
+                    Console.WriteLine();
+                }
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
