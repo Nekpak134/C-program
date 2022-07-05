bebb7fa8659c307cb3fe49422032bcd7e910fde4
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-07-05 15:55:42 +0530

    Add files via upload

---
+++ aryr list string.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        private static object myNumbers;
+
+        static void Main(string[] args)
+        {
+            int [] Numbers = { 101, 102, 103, 104, 104, 105, 106, 107, 108, 109, 110 };
+
+            //Console.WriteLine(Numbers.Max());
+          //  Console.WriteLine(Numbers.Min());
+            Console.WriteLine(Numbers.Sum());
+            
+            Console.ReadLine();
+        }
+    }
+}
+            
+        
+    
+
