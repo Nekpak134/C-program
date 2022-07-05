d8d6e80de183975047a56ef5e7124ce2540a35ad
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-07-05 15:57:42 +0530

    Add files via upload

---
+++ methad simple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        void N1(int a)
+        {
+            Console.WriteLine("nekpal: "+a);
+        }
+        void N1(int a,int b)
+        {
+            Console.WriteLine("nekpal: "+a+ "and "+b);
+        }
+        static void Main(string[]args)
+        {
+            Program obj = new Program();
+               obj.N1(20);
+               obj.N1(10,20);
+            Console.ReadLine();
+        }
+    }
+}
+            
+        
+    
+
---
+++ overloding string.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    class program
+    {
+        void M1(string x)
+        {
+            Console.WriteLine("nekpal: " + x);
+        }
+        void M1(string y, string z)
+        {
+            Console.WriteLine("nekpal: " + y  + z);
+        }
+        static void Main(string[] args)
+        {
+            program obj = new program();
+                  obj.M1("rahul");
+            obj.M1("raju  ", "sachin");
+            Console.ReadLine();
+        }
+
+    }
+}
+            
+        
+    
+
