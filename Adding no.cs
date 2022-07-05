9947e07edd780a4d8bfc3591ec1e4c1119f90201
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-07-05 15:53:31 +0530

    Add files via upload

---
+++ Adding no.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 20;
+            if (time < 20)
+            {
+                Console.WriteLine("Good morning");
+            }
+            else if (time <10)
+            {
+                Console.WriteLine("good eveing");
+
+            }
+            else
+            {
+                Console.WriteLine("good nt");
+            }
+            Console.ReadLine();
+        }
+    }
+}
